35fb87ccc4e1a100683db18f03817f3da10dfa44
Isaac Valdez <isaacv123456789@gmail.com>
2023-05-18 18:16:03 -0500

    xatlasjs imported as type so parcel doesn't parse the source

--- src/surfaces/processors/texturing/uv-unwrapping/algorithms/xatlas.ts
+++ src/surfaces/processors/texturing/uv-unwrapping/algorithms/xatlas.ts
@@ -1,10 +1,11 @@
-import { XAtlasAPI } from 'xatlasjs-esm'
+import type { XAtlasAPI } from 'xatlasjs-esm'
 import { SurfaceUVUnwrappingAlgorithm } from '../algorithm.js';
 import { Vec2 } from 'playcanvas-extended';
 import { indicesArrayType } from '../../../../../utils/indices-array.js';
 
 const promise_xAtlas = new Promise<XAtlasAPI>(resolve => {
-    const xAtlas: XAtlasAPI = new XAtlasAPI(
+    const XAtlasAPIClass = (globalThis as any)["XAtlasAPI"] as typeof XAtlasAPI
+    const xAtlas: XAtlasAPI = new XAtlasAPIClass(
         () => resolve(xAtlas),
         (path, dir) => {
             if (path === "xatlas.wasm") return "libs/" + path;
