b4f62fb4f4700c000afce50748c06ae074a857c5
Isaac Valdez <isaacv123456789@gmail.com>
2023-09-12 17:11:49 -0500

    metaplane vector sampling

--- src/solids/metasolids/metaplane.ts
+++ src/solids/metasolids/metaplane.ts
@@ -1,5 +1,5 @@
 import { BoundingBox, Vec2, Vec3 } from "playcanvas-extended";
-import { Field, FieldsPointMapped, FieldsPointOptional, MultiObjectsInfluencesGroupsDefault } from "../../fields/index.js";
+import { Field, FieldsPointMapped, FieldsPointOptional, MultiObjectsInfluencesGroupsDefault, field_point_new } from "../../fields/index.js";
 import { VolumeSurfacesKey, meshing } from "../../surfaces/index.js";
 import { TextureLocation, TextureSamplingContext } from "../../textures/texture.js";
 import { Sign } from "../../utils/sign.js";
@@ -9,6 +9,8 @@ import { SignField } from "../../fields/fields/sign.js";
 import { VolumeSolidsKey } from "../volume-solids.js";
 import { MultiObjectsGroupsTemplate } from "../../paradigm/trees/index.js";
 import { Cloneable, clone, makeClone } from "../../utils/cloneable.js";
+import { FieldPointVector, FieldPointVectorContainerStatic, field_point_vector_fill, field_point_vectorized_new } from "../../fields/vectorized/index.js";
+import { vectorized } from "vectorized-functions";
 
 export type MetaPlaneSample = MetaSolidSample & {
     side: Sign
@@ -61,7 +63,7 @@ export class MetaPlane<
     )
 
     constructor(
-        public maxDomain: {
+        public area: {
                 offset: Vec2,
                 size: Vec2
             } = {
@@ -81,15 +83,15 @@ export class MetaPlane<
                 TextureContext,
                 VolumeContext
             >(
-                makeClone(this.maxDomain),
+                makeClone(this.area),
             )
     }
 
     init(context: Context): void {
         const texture = context[MetaSolidSamplingContext_Texture]?.item
 
-        const xy_offset = this.maxDomain.offset
-        const xy_size = this.maxDomain.size
+        const xy_offset = this.area.offset
+        const xy_size = this.area.size
 
         const resolution = 32
 
@@ -146,14 +148,15 @@ export class MetaPlane<
         }
     }
 
+    @vectorized(MetaPlane.sample_vectorized)
     sample(location: Location, context: Context): Sample {
         const side = Math.sign(location.p.z) as Sign
         const gradient = new Vec3(0, 0, side)
         const distance = Math.abs(location.p.z)
 
         const uv = new Vec2(
-            ((location.p.x - this.boundingBox.center.x) / this.boundingBox.halfExtents.x) + 0.5,
-            ((location.p.y - this.boundingBox.center.y) / this.boundingBox.halfExtents.y) + 0.5
+            (location.p.x - this.area.offset.x) / this.area.size.x,
+            (location.p.y - this.area.offset.y) / this.area.size.y
         )
 
         return {
@@ -163,5 +166,86 @@ export class MetaPlane<
             side,
             uv
         } as unknown as Sample
+    }
+
+    private static sample_vectorized<
+            InfluenceGroup extends MultiObjectsGroupsTemplate = MultiObjectsInfluencesGroupsDefault,
+            TxLocation extends TextureLocation = TextureLocation,
+            TxSample extends MetaSolidTxSample = MetaSolidTxSample,
+            Location extends MetaSolidLocation = MetaSolidLocation,
+            Sample extends MetaPlaneSample = MetaPlaneSample,
+            OuterSampleProcessingContextT = any,
+            TextureContext extends
+                TextureSamplingContext<MetaSolidTxLocation<Location, TxLocation>> =
+                TextureSamplingContext<MetaSolidTxLocation<Location, TxLocation>>,
+            VolumeContext extends
+                MetaSolidVolumeSamplingContext<InfluenceGroup, TxLocation, Location, OuterSampleProcessingContextT, TextureContext> =
+                MetaSolidVolumeSamplingContext<InfluenceGroup, TxLocation, Location, OuterSampleProcessingContextT, TextureContext>,
+            Context extends
+                MetaSolidShapeSamplingContext<InfluenceGroup, TxLocation, TxSample, Location, OuterSampleProcessingContextT, TextureContext, VolumeContext> =
+                MetaSolidShapeSamplingContext<InfluenceGroup, TxLocation, TxSample, Location, OuterSampleProcessingContextT, TextureContext, VolumeContext>,
+        >(
+            this: MetaPlane<
+                    InfluenceGroup,
+                    TxLocation,
+                    TxSample,
+                    Location,
+                    Sample,
+                    OuterSampleProcessingContextT,
+                    TextureContext,
+                    VolumeContext,
+                    Context
+                >,
+            locations: FieldPointVector<Location, FieldPointVectorContainerStatic>,
+            context: Context
+        ): FieldPointVector<Sample, FieldPointVectorContainerStatic> {
+        const location_p = locations.p
+        let location_p_i = 0,
+            location_p_x: number,
+            location_p_y: number,
+            location_p_z: number
+        
+        const area_offset_x = this.area.offset.x
+        const area_offset_y = this.area.offset.y
+        const area_size_x = this.area.size.x
+        const area_size_y = this.area.size.y
+        
+        const length = location_p.length / 3
+        
+        const results = field_point_vectorized_new<Sample, FieldPointVectorContainerStatic>(
+            this.field.elementType,
+            length,
+            false
+        )
+
+        const results_side = results.side
+        const results_gradient = results.gradient
+        const results_distance = results.distance
+        const results_uv = results.uv
+
+        field_point_vector_fill(
+            this.field.elementType,
+            MetaSolidVolume.defaultFields.parametersIn.elementType,
+            results,
+            MetaSolidVolume.defaultParameters
+        )
+
+        let results_uv_i = 0
+        
+        for (let i = 0; i < length; i++) {
+            location_p_x = location_p[location_p_i++]
+            location_p_y = location_p[location_p_i++]
+            location_p_z = location_p[location_p_i]
+            
+            results_gradient[location_p_i] = results_side[i] = Math.sign(location_p_z)
+            location_p_i++
+
+            results_distance[i] = Math.abs(location_p_z)
+
+            results_uv[results_uv_i++] = (location_p_x - area_offset_x) / area_size_x
+            results_uv[results_uv_i++] = (location_p_y - area_offset_y) / area_size_y
+        }
+        
+        return results
     }
 }
